89622781bfaf521eec1a56217224914f299d457f
Pokobuild <159025506+Pokobuild@users.noreply.github.com>
2024-02-08 23:13:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -23,11 +23,12 @@ adjustCanvasSize(canvas, 500, 500); // Set this to your desired CSS display size
 
 // Object to keep track of the selected parts
 const selectedParts = {
+    hat: 'images/hat/hat6.png'
     bg: 'images/bg/bg1.png',
     head: 'images/head/head.png',
     eyes: 'images/eyes/eyes1.png',
     mouth: 'images/mouth/mouth1.png',
-    hat: 'images/hat/hat6.png'
+    
 };
 
 function drawPart(partPath) {
